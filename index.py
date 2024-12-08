6d437953cb4818310d78762a1229823152a8ba37
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-08 13:28:00 -0700

    create basic post endpoint

--- index.py
+++ index.py
@@ -5,9 +5,17 @@ app = FastAPI()
 
 @app.get("/payments")
 def get_payments():
-    collection = get_collection("payment_records")
-    payments = list(collection.find())
-    for payment in payments:
-      payment['_id'] = str(payment['_id'])
-    
-    return {"payments": payments}
+  collection = get_collection("payment_records")
+  payments = list(collection.find())
+  for payment in payments:
+    payment['_id'] = str(payment['_id'])
+  
+  return {"payments": payments}
+
+@app.post("/payments")
+def create_payment(payment: dict):
+  collection = get_collection("payment_records")
+  result = collection.insert_one(payment)
+  
+  return {"inserted_id": str(result.inserted_id)}
+
