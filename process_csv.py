89c7ccef61853511f3377b2d494661a9cea5647a
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-07 22:18:42 -0700

    data type conversion

--- process_csv.py
+++ process_csv.py
@@ -2,4 +2,8 @@ import pandas as pd
 from pymongo import MongoClient
 from datetime import datetime
 
-df = pd.read_csv('payment_information')
+df = pd.read_csv('payment_information.csv')
+print(df.head())
+
+df['payee_added_date_utc']=pd.to_datetime(df['payee_added_date_utc'], errors='coerce')
+df['payee_due_date'] = pd.to_datetime(df['payee_due_date'], errors='coerce')
