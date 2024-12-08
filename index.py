03af58f46f637457901123459cb5d78550eabfc6
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-08 13:37:05 -0700

    Add payment calculation and status update functions

--- index.py
+++ index.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from datetime import datetime
 from db import get_collection
 
 app = FastAPI()
+
+
+def calculate_total_due(payment):
+  discount_amount=(payment['due_amount']*payment['discount_percent'])/100
+  tax_amount = (payment['due_amount']*payment['tax_percent'])/100
+  total_due = payment['due_amount'] - discount_amount + tax_amount
+  return total_due
+
+def update_payment_status(payment):
+  today = datetime.utcnow().date()
+  payee_due_date = datetime.utcfromtimestamp(payment['payee_due_date']).date()
+
+  if payee_due_date == today:
+    payment['payee_payment_status'] = "due_now"
+  elif payee_due_date< today:
+    payment['payee_payment_status'] = "overdue"
+
+  return payment
 
 @app.get("/payments")
 def get_payments():
