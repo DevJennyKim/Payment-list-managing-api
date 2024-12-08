23cf0f4becd8cbceb0896faef46f6b513ef41ffa
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-12-08 13:10:38 -0700

    Setup MongoDB connection and get_payments endpoint

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from db import get_collection
 
 app = FastAPI()
 
+@app.get("/payments")
+def get_payments():
+    collection = get_collection("payment_records")
+    payments = list(collection.find())
+    for payment in payments:
+      payment['_id'] = str(payment['_id'])
+    
+    return {"payments": payments}
